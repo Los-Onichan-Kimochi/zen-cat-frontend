ef4135a689643fbcf353a63386b87b56228441a0
Litus <zegarraward@gmail.com>
2025-05-01 15:18:15 -0500

    setting prettier

--- ZenCat-Front-Admin/eslint.config.js
+++ ZenCat-Front-Admin/eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -25,4 +25,37 @@ export default tseslint.config(
       ],
     },
   },
-)
+  prettierConfig,
+  {
+    rules: {
+      'max-len': ['warn', { code: 80 }],
+      // Deshabilita las reglas innecesarias de importación de React
+      'react/react-in-jsx-scope': 'off',
+      // Asegura que se utilicen puntos y comas
+      semi: ['error', 'always'],
+      // Desactiva la regla que requiere un salto de línea antes de return
+      'newline-before-return': 'off',
+      // Limita las líneas vacías
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+      // No permite múltiples declaraciones de variables en una sola línea
+      'one-var': ['error', 'never'],
+      // Establece el indentado a 4 espacios
+      indent: ['error', 2],
+      // Requiere llaves en todas las estructuras de control multilínea
+      curly: ['error', 'multi-line'],
+      // Desactiva anotaciones de tipos triviales
+      '@typescript-eslint/no-inferrable-types': [
+        'error',
+        { ignoreParameters: true },
+      ],
+      // Prohíbe el uso de == y !=, excepto para comparar con null
+      eqeqeq: ['error', 'always', { null: 'ignore' }],
+      // Requiere el uso de comillas simples
+      quotes: ['error', 'single'],
+      // Asegura que haya espacios alrededor de los operadores
+      'space-infix-ops': 'error',
+      // Prohíbe el uso de paréntesis en constructores sin argumentos
+      'new-cap': ['error', { capIsNew: true }],
+    },
+  },
+);
