73f061fd628a317c6df07bfb91a31d224a03cbb0
fapi19 <fabipra19@gmail.com>
2025-05-15 22:09:24 -0500

    Updating

--- ZenCat-Front-Admin/src/main.tsx
+++ ZenCat-Front-Admin/src/main.tsx
@@ -2,13 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 
 import { routeTree } from './routeTree.gen'
 
 const router = createRouter({ routeTree })
 
+
 const queryClient = new QueryClient()
+
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
 
 declare module '@tanstack/react-router' {
   interface Register {
